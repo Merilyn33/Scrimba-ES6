045016f7752b8674ff56eb78e46e8908ec8c5b87
Merilyn Merisalu <175918@taltech.ee>
2022-09-07 22:38:28 +0300

    for of loop

---
+++ Challenges/object_literals/js/index.js
@@ -0,0 +1,12 @@
+
+/* function addressMaker(address) {
+    const {city, state} = address;
+    const newAddress = {
+        
+        city,
+        state,
+        country: "Estonia"
+    };
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`);
+}
+addressMaker({city:"Tallinn", state:"Harjumaa" }); */
--- Practice/js/index.js
+++ Practice/js/index.js
@@ -1,7 +1,7 @@
 
-const word1 = 'Merilyn';
+/* const word1 = 'Merilyn';
 const word2 = 'Merisalu';
-
+ */
 //const fullName = word1 + ' ' + word2;
 /* const fullName = `${word1} ${word2}`
 console.log(fullName); */
@@ -14,23 +14,23 @@ console.log(example); */
 /* const example = 'Hello, \n' + 'world!';
 const sentence = document.getElementById("example"); */
 
-const sentence = document.getElementById("example");
+/* const sentence = document.getElementById("example");
 
 const example = `${word1}
 ${word2}`
-sentence.innerText += example;
+sentence.innerText += example; */
 
 // Destructuring objects
 
-const personalInformation = {
+/* const personalInformation = {
     firstName: "Merilyn",
     lastName: "Merisalu",
     city: "Tallinn",
     state: "Harjumaa",
     zipcode: "10117"
 };
-
-const {firstName:fn, lastName:ln} = personalInformation;
+ */
+/* const {firstName:fn, lastName:ln} = personalInformation;
 console.log(`${fn} ${ln}`);
 
 const player = {
@@ -40,9 +40,42 @@ const player = {
         city: "Los Angeles",
     }     
     
-};
+}; */
 
 //console.log(player.fullName);
-const {fullName, club:c, address:{city}} = player;
+// const {fullName, club:c, address:{city}} = player;
 //console.log(`${fullName} plays for ${c}`);
-console.log(`${fullName} lives in ${city}`);
+// console.log(`${fullName} lives in ${city}`);
+
+// Destructing Arrays
+
+/* let [firstName, middleName, lastName] = ["Merilyn", "Coding God", "Merisalu"];
+middleName = "Writer";
+console.log(lastName);
+console.log(middleName);
+ */
+
+// Object Literals
+/* 
+function addressMaker(city, state) {
+    const newAddress = {city, state};
+    console.log(newAddress);
+}
+
+addressMaker("Tallinn", "Harjumaa"); */
+
+// For of loop
+
+/* let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    total += income;
+}
+console.log(total); */
+
+const fullName = "Merilyn Merisalu";
+
+for(let char of fullName) {
+    console.log(char);
+}
